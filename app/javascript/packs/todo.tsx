70b4cd6ecd0883065f0434d683572a98616ce15f
Zack Hovatter <zackhovatter@gmail.com>
2019-10-07 09:54:37 -0400

    Initial commit

---
+++ app/javascript/packs/todo.tsx
@@ -0,0 +1,23 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+type HelloProps = {
+  name: string;
+};
+
+const App: React.FC<HelloProps> = ({ name }) => {
+  return (
+    <div>Hello {name}!</div>
+  )
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
